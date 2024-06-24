c78c54ded056f90744ed12e9e8de3148caab7862
Levi Zitting <me@levizitting.com>
2024-06-23 21:38:21 -0500

    chore: fix linter issues

--- updateTrackingLinks.ts
+++ updateTrackingLinks.ts
@@ -1,15 +1,17 @@
 // Why: this file ensures the tracking links are functionings by 
 // replacing the '&#x27;' character substitue (generated during the react email export process) with '"' 
 
-var fs = require('fs')
+import fs from 'fs';
 
 fs.readFile('./out/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(/&#x27;/g, '"');
+  const result = data.replace(/&#x27;/g, '"');
 
   fs.writeFile('./out/index.html', result, 'utf8', function (err) {
-     if (err) return console.log(err);
+     if (err) {
+      return console.log(err);
+     }
   });
 });
