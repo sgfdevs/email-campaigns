c96dd233f19185a8a0fcbcca27abc7ab04529944
Levi Zitting <me@levizitting.com>
2024-06-23 22:00:54 -0500

    feat: add prettier for formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 // @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   ...tseslint.configs.recommended,
 );
