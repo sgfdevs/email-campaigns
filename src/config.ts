fe555f8c035e0a465d905e2d7119416afe651ad4
Levi Zitting <me@levizitting.com>
2024-08-18 20:07:03 -0500

    fix: local asset preview

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import { _throw, parseNumber } from './util';
 
-export const ASSET_BASE_URL = 'https://newsletter.sgf.dev/uploads';
-
 export const SGF_DEVS_DISCORD_LINK = 'https://discord.com/invite/VNNJwJk';
 export const SGF_DEVS_PROFILE_LINK = 'https://sgf.dev/register';
 export const SGF_DEVS_TWITCH_LINK = 'https://www.twitch.tv/sgfdevs';
 export const CENTRAL_TIMEZONE = 'America/Chicago';
+
+export const ASSET_BASE_URL =
+    process.env.ASSET_BASE_URL ?? _throw('ASSET_BASE_URL env var not set');
 
 export const SGF_MEETUP_API_BASE_URL =
     process.env.SGF_MEETUP_API_BASE_URL ??
